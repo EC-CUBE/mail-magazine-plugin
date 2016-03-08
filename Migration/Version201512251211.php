fc01c8ee2e67f7123292f190684c8ff4dfda2d0f
ryo-endo <to.endo.dev@gmail.com>
2016-03-08 19:16:30 +0900

    リリース後に行われた旧マイグレーションファイルへの変更を、新しいマイグレーションファイルに移植した。

--- Migration/Version201512251211.php
+++ Migration/Version201512251211.php
@@ -26,8 +26,16 @@ class Version201512251211 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->createPlgMailmagaCustomer($schema);
+
         $table = $schema->getTable('plg_send_history');
-        $table->changeColumn('send_id', array('autoincrement' => true));
+        $table->addIndex(
+            array('send_id')
+        );
+
+        if ($this->connection->getDatabasePlatform()->getName() == "mysql") {
+            $table->changeColumn('send_id', array('autoincrement' => true));
+        }
     }
 
     /**
@@ -36,5 +44,58 @@ class Version201512251211 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $schema->dropTable('plg_mailmaga_customer');
+        $schema->dropSequence('plg_mailmaga_customer_id_seq');
+    }
+
+    /***
+     * plg_mailmaga_customerテーブルの作成
+     *
+     * CREATE SEQUENCE plg_mailmaga_customer_id_seq;
+     * CREATE TABLE plg_mailmaga_customer (
+     *   id integer DEFAULT nextval('plg_mailmaga_customer_id_seq'::regclass) NOT NULL PRIMARY KEY,
+     *   customer_id integer NOT NULL,
+     *   mailmag_flg smallint DEFAULT 0 NOT NULL,
+     *   del_flg smallint DEFAULT 0 NOT NULL,
+     *   create_date timestamp(0) without time zone NOT NULL,
+     *   update_date timestamp(0) without time zone NOT NULL
+     *);
+     *
+     * @param Schema $schema
+     */
+    protected function createPlgMailmagaCustomer(Schema $schema)
+    {
+        $table = $schema->createTable("plg_mailmaga_customer");
+        $table->addColumn('id', 'integer', array(
+            'autoincrement' => true,
+        ));
+
+        $table->addColumn('customer_id', 'integer', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('mailmaga_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('del_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('create_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('update_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->setPrimaryKey(array('id'));
     }
 }
