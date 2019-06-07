881925204c455e11d58d32dbfb206187b67369cc
junpeko5 <junpeko5@gmail.com>
2019-06-07 17:33:08 +0900

    関数の引数をリファクタリング。コメントを追加。

--- Resource/doctrine/migration/Version201906031100.php
+++ Resource/doctrine/migration/Version201906031100.php
@@ -12,19 +12,22 @@ class Version201906031100 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
+        // 3.0.8以前はgetInstance関数が無いことを考慮する
         if (version_compare(Constant::VERSION, '3.0.9', '>=')) {
             $app = Application::getInstance();
             $entityManager = $app['orm.em'];
-            // Retrieve PDO instance
             $pdo = $entityManager->getConnection()->getWrappedConnection();
         } else {
+            // 直接DBに接続してPDOを生成
             $pdo = $this->getPDO();
         }
 
+        // search_dataをserializeされたデータからjson形式に変換する
         $stmt = $pdo->prepare('SELECT send_id, search_data FROM plg_send_history;');
         $stmt->execute();
         foreach ($stmt as $row) {
-            $formData = $this->unserializeWrapper($row, 'search_data');
+            $formData = $this->unserializeWrapper(base64_decode($row['search_data']));
+
             // unserializeしたデータからJSONに変換
             $formDataArray = $formData;
             $formDataArray['pref'] = ($formData['pref'] != null) ? $formData['pref']->toArray() : null;
@@ -50,8 +53,14 @@ class Version201906031100 extends AbstractMigration
     {
     }
 
-    private function unserializeWrapper($dataArray, $key) {
-        $serializedData = base64_decode($dataArray[$key]);
+    /**
+     * 互換性のないEntityを取り除いた状態でUnserialiseを実行する
+     * Member,Customerは "__php_incomplete_class"となる
+     *
+     * @param array $serializedData Base64でエンコードされたシリアライズデータ
+     * @return mixed unserializeしたデータ
+     */
+    private function unserializeWrapper($serializedData) {
         $serializedData = str_replace('DoctrineProxy\__CG__\Eccube\Entity\Member', '__Workaround_\__CG__\Eccube\Entity\Member', $serializedData);
         $serializedData = str_replace('DoctrineProxy\__CG__\Eccube\Entity\Customer', '__Workaround_\__CG__\Eccube\Entity\Customer', $serializedData);
         return unserialize($serializedData);
